58dda46bb6874a8cc7993ff90f5dd91a52411bc1
hardikdavda5677 <100426792+hardikdavda5677@users.noreply.github.com>
2023-01-18 21:34:23 +0530

    new design

--- js/custom.js
+++ js/custom.js
@@ -55,32 +55,6 @@ $(document).ready(function(){
   });
 });
 
-
-$(document).ready(function(){
-$(function () {
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
-      items: 1,
-      nav: true,
-      loop: false,
-      mouseDrag: false,
-      dots: false,
-      onInitialized: counter, //When the plugin has initialized.
-      onTranslated: counter //When the translation of the stage has finished.
-  });
-
-  function counter(event) {
-      var element = event.target;         // DOM element, in this example .owl-carousel
-      var items = event.item.count;     // Number of items
-      var item = event.item.index + 1;     // Position of the current item
-
-      // it loop is true then reset counter from 1
-      if (item > items) {
-          item = item - items
-      }
-      $('.counter').html("Showing " + item + " to " + items + " of " + items + " entries")
-  }
-});
-});
+ 
 })(jQuery);  
 
